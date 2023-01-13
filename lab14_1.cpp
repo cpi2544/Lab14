e486194ab3a82fc76a3182e3bea1862a6477a4f5
cpi2544 <pokpong1789@gmail.com>
2023-01-13 15:14:33 +0700

    lab14.cpp completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,43 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum1 = 0;
+    double sum2 = 0;
+    double max = A[0],min = A[0];
+    double G = 1;
+    double H = 0;
+    double Q;
+
+    for(int i=0;i<N;i++){
+        sum1 += A[i];
+    }
+    B[0] = sum1/N;
+
+    for(int j=0;j<N;j++){
+        sum2 += pow(A[j]-B[0],2);
+        Q = sum2/N;
+    }
+    B[1] = sqrt(Q);
+
+    for(int k=0;k<N;k++){
+        G *= A[k];
+    }
+    B[2] = pow(G,(1.0/N));
+
+    for(int l=0;l<N;l++){
+        H += (1/A[l]);
+    }
+    B[3] = N/H;
+
+    for(int m=0;m<N;m++){
+        if (A[m] > max) 
+            max = A[m];
+        if (A[m] < min)
+            min = A[m];   
+
+    }
+    B[4] = max;
+    B[5] = min;
+}
